443521eed0da990d8b0a8133cd5bb49a91a2ee7e
sstol <sstol@rbbn.com>
2022-02-22 19:47:18 +0200

    Init commit

---
+++ bash-menu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#process arguments
+while test $# -gt 0; do
+  case "$1" in
+  -h | --help)
+    echo "help"
+    exit 1
+    ;;
+  -s | --stop)
+    echo "stop"
+    exit 0
+    ;;
+  -r | --refresh)
+    echo "refresh"
+    exit 0
+    ;;
+  *)
+    echo "other command"
+    break
+    ;;
+  esac
+done
